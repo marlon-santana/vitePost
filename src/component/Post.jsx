99a6f009dbeaea9c765afdc8dd1a878b2709edb8
Marlon Oliveira <marlon.oliveira@somagrupo.com.br>
2022-08-15 22:58:37 -0300

    feat/add key in render

--- src/component/Post.jsx
+++ src/component/Post.jsx
@@ -45,20 +45,14 @@ export function Post({ author, content, publishedAt }) {
       <div className={styles.content}>
         {content.map((line) => {
           if (line.type === "paragraph") {
-            return <p>{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
           } else if (line.type === "link") {
             return (
-              <p>
+              <p ky={line.content}>
                 <a src="#">{line.content}</a>
               </p>
             );
-          } else if (line.type === "hash") {
-            return (
-              <p>
-                <a src="#">{line.content}</a>
-              </p>
-            );
-          }
+          } 
         })}
       </div>
 
@@ -75,7 +69,7 @@ export function Post({ author, content, publishedAt }) {
         </footer>
       </form>
       {comments.map((comment) => {
-        return <Comment content={comment} />;
+        return <Comment key={comment} content={comment} />;
       })}
     </article>
   );
